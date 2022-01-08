fbd0c36c40d18f3ac3474954a897c11bff0d6b3e
Gwenaël Magnenat <g.magnenat@gmail.com>
2022-01-08 18:59:43 +0100

    Initialise app and build the header

---
+++ src/Header.js
@@ -0,0 +1,37 @@
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import HeaderOption from './HeaderOption';
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className='header__left'>
+        <img src='linkedin_logo.svg' alt='' />
+
+        <div className='header__search'>
+          <SearchIcon />
+          <input type='text' />
+        </div>
+      </div>
+
+      <div className='header__right'>
+        <HeaderOption Icon={HomeIcon} title='Home' />
+        <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
+        <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
+        <HeaderOption Icon={ChatIcon} title='Messaging' />
+        <HeaderOption Icon={NotificationsIcon} title='Notifications' />
+        <HeaderOption
+          avatar='https://media-exp1.licdn.com/dms/image/C4D03AQFuYg0IP7pDSw/profile-displayphoto-shrink_800_800/0/1610912942072?e=1646870400&v=beta&t=lmxaCtsHky89M1b9cmCX8Ywxjs7_0dXO8tGEZQCkr6w'
+          title='me'
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
