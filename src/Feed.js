b193457c563777c5fc68c3a6a49bc7ac09f3b756
Gwenaël Magnenat <g.magnenat@gmail.com>
2022-01-09 08:56:33 +0100

    Add a feed component and styles

---
+++ src/Feed.js
@@ -0,0 +1,35 @@
+import './Feed.css';
+import CreateIcon from '@material-ui/icons/Create';
+import InputOption from './InputOption';
+import ImageIcon from '@material-ui/icons/Image';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
+
+function Feed() {
+  return (
+    <div className='feed'>
+      <div className='feed__inputContainer'>
+        <div className='feed__input'>
+          <CreateIcon />
+          <form>
+            <input type='text' />
+            <button type='submit'>Send</button>
+          </form>
+        </div>
+        <div className='feed__inputOptions'>
+          <InputOption Icon={ImageIcon} title='Photo' color='#70b5f9' />
+          <InputOption Icon={SubscriptionsIcon} title='Photo' color='#e7a33e' />
+          <InputOption Icon={EventNoteIcon} title='Photo' color='#c0cbcd' />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title='Photo'
+            color='#7fc15e'
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
