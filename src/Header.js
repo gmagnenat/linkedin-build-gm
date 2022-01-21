405e36508d5492971b8c288b454da8d998e5c05e
Gwenaël Magnenat <g.magnenat@gmail.com>
2022-01-21 14:30:08 +0100

    Fix user avatar in the header

--- src/Header.js
+++ src/Header.js
@@ -9,43 +9,49 @@ import HeaderOption from './HeaderOption';
 import { useDispatch, useSelector } from 'react-redux';
 import { auth } from './firebase';
 import { logout, selectUser } from './features/userSlice';
+import { Avatar } from '@material-ui/core';
 
 function Header() {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const logoutOfApp = () => {
-    dispatch(logout());
-    auth.signOut();
-  };
+	const logoutOfApp = () => {
+		dispatch(logout());
+		auth.signOut();
+	};
 
-  const user = useSelector(selectUser);
+	const user = useSelector(selectUser);
 
-  return (
-    <div className='header'>
-      <div className='header__left'>
-        <img src='linkedin_logo.svg' alt='' />
+	return (
+		<div className="header">
+			<div className="header__left">
+				<img src="linkedin_logo.svg" alt="" />
 
-        <div className='header__search'>
-          <SearchIcon />
-          <input type='text' placeholder='Search' />
-        </div>
-      </div>
+				<div className="header__search">
+					<SearchIcon />
+					<input type="text" placeholder="Search" />
+				</div>
+			</div>
 
-      <div className='header__right'>
-        <HeaderOption Icon={HomeIcon} title='Home' />
-        <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
-        <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
-        <HeaderOption Icon={ChatIcon} title='Messaging' />
-        <HeaderOption Icon={NotificationsIcon} title='Notifications' />
-        <HeaderOption
-          avatar={user?.photoUrl}
-          title={user?.email[0]}
-          onClick={logoutOfApp}
-        />
-        <button></button>
-      </div>
-    </div>
-  );
+			<div className="header__right">
+				<HeaderOption Icon={HomeIcon} title="Home" />
+				<HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+				<HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+				<HeaderOption Icon={ChatIcon} title="Messaging" />
+				<HeaderOption Icon={NotificationsIcon} title="Notifications" />
+				{!user?.photoUrl ? (
+					<Avatar onClick={logoutOfApp} src={user?.photoUrl}>
+						{user?.displayName[0]}
+					</Avatar>
+				) : (
+					<HeaderOption
+						avatar={user?.photoUrl}
+						onClick={logoutOfApp}
+						title="me"
+					/>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default Header;
